08048942eeb9d887677a9c21fd907bdac9f1fa62
Dmitriy Khraltsov <life608@yandex.ru>
2025-04-09 15:38:56 +0300

    first comment

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,10 @@
+const cookie = document.getElementById('cookie')
+
+cookie.onclick = function(){
+    const counter = document.getElementById('clicker__counter')
+    counter.textContent = Number(counter.textContent) + 1
+    setTimeout(()=>{
+        cookie.width = 200
+    }, 100)
+    cookie.width = 150
+}
